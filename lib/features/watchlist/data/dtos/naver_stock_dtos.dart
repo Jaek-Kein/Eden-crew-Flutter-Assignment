7e856f9c5f76f1302908c3b127682834fef138c1
Jaewook Kim <yearsingle@naver.com>
2026-06-16 07:47:37 +0900

    feat: implement NaverChartMetadataDTO

--- lib/features/watchlist/data/dtos/naver_stock_dtos.dart
+++ lib/features/watchlist/data/dtos/naver_stock_dtos.dart
@@ -96,9 +96,10 @@ class NaverChartMetadataDto {
   });
 
   factory NaverChartMetadataDto.fromJson(Map<String, dynamic> json) {
-    // TODO(assignment): Map the chart metadata payload into this DTO.
-    throw UnimplementedError(
-      'TODO(assignment): implement NaverChartMetadataDto.fromJson',
+  return NaverChartMetadataDto(
+      symbol: _readString(json['symbolCode']),
+      stockName: _readString(json['stockName']),
+      stockExchangeNameKor: _readString(json['stockExchangeNameKor']),
     );
   }
 
